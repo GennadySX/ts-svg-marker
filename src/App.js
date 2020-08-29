1924cf3057d67c0d0a55b59049bae5412d674439
GennadySX <htcucell@gmail.com>
2020-08-29 22:00:35 +0300

    fs

--- src/App.js
+++ src/App.js
@@ -3,161 +3,143 @@ import {SVG} from '@svgdotjs/svg.js'
 import './App.css';
 
 const cd = [
-  {
-    title: 'Казань'
-  },
-  {
-    title: 'Кулаево'
-  },
-  {
-    title: 'Большой \n маляш'
-  },
-  {
-    title: 'Мамыловка'
-  },
-  {
-    title: 'Елабуга'
-  },
-  {
-    title: 'Набережные \n челны'
-  },
-  {
-    title: 'Кулаево 1'
-  },
-  {
-    title: 'Большой \n маляш 1'
-  },
-  {
-    title: 'Мамыловка 1'
-  },
-  {
-    title: 'Елабуга 1'
-  },
-  {
-    title: 'Набережные \n челны 1'
-  },
+    {
+        title: 'Казань'
+    },
+    {
+        title: 'Кулаево'
+    },
+    {
+        title: 'Большой \n маляш'
+    },
+    {
+        title: 'Мамыловка'
+    },
+    {
+        title: 'Елабуга'
+    },
+    {
+        title: 'Набережные \n челны'
+    },
+    {
+        title: 'Кулаево 1'
+    },
+    {
+        title: 'Большой \n маляш 1'
+    },
+    {
+        title: 'Мамыловка 1'
+    },
+    {
+        title: 'Елабуга 1'
+    },
+    {
+        title: 'Набережные \n челны 1'
+    },
 
 ]
 
 
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
+    constructor(props) {
+        super(props);
+    }
 
 
     componentDidMount() {
-      this.drawIn()
+        this.drawIn()
     }
-
-
 
 
     circleMaker(title) {
-      const  draw = SVG().addTo('.intercity').size(550, 500)
+        const draw = SVG().addTo('.intercity').size(550, 500)
+        draw.circle(40).fill('#c21235').move(40, 10)
+        draw.circle(50).fill('none').move(35, 5)
+            .stroke({color: '#c21235', width: 5, linecap: 'round'})
+        draw.text(title).move(40, 60).font({weight: 'bold'})
 
-      draw.circle(40).fill('#c21235').move(40, 10)
-      draw.circle(50).fill('none').move(35, 5)
-          .stroke({ color: '#c21235', width: 5, linecap: 'round' })
-      draw.text(title).move(40, 60).font({weight: 'bold'})
-
-      draw.line(20, 100, 420, 100).move(60, 30)
-          .stroke({ color: 'rgba(194,18,53,0.68)', width: 10 })
+        draw.line(20, 100, 420, 100).move(60, 30)
+            .stroke({color: 'rgba(194,18,53,0.68)', width: 10})
     }
 
     drawIn() {
-      const draw = SVG().addTo('.intercity').size(1750, 1000)
-      const leftBorder = '-20 -20 0 0 -20, 20 v100 a20, 20 -20 0 0 20, '
-      const rightBorder = ' 20 0 0 1 20, 20 v100 a20,  20 0 0 1 -20,'
-      const line = (min = false) => `20 h${min ? '-' : ''}450 a20,`
-      const end = ' 0 v0 a20, 20 0 0 1 20,-20 z'
+
+        const W_Element = 120
+        const V_Block = 120
+        const V_Text = 50
 
 
-      const path = draw.path('M0,'+line()
-          + rightBorder+ line(true)
-          + leftBorder+line()
-          + rightBorder+line(true)
-          + end)
-      path.fill('none').move(50, 30)
-      path.stroke({ color: 'rgba(194,18,53,0.68)', width: 10})
+        const draw = SVG().addTo('.intercity').size(1750, 1000)
+        const leftBorder = '-20 -20 0 0 -20, 20 v100 a20, 20 -20 0 0 20, '
+        const rightBorder = ' 20 0 0 1 20, 20 v100 a20,  20 0 0 1 -20,'
+        const line = (min = false, size = 450) => `20 h${min ? '-' : ''}${size} a20,`
+        const end = ' 0 v0 a20, 20 0 0 1 20,-20 z'
 
 
+        const path = draw.path('M0,' + line()
+            + rightBorder + line(true)
+            + leftBorder + line(false)
+            + rightBorder + line(true)
+            + leftBorder + line(false)
+            + end)
 
+        path.fill('none').move(50, 30)
+        path.stroke({color: 'rgba(194,18,53,0.68)', width: 10})
 
-    cd.map((map, i) => {
-        if (i === 0) {
-          draw.circle(40).fill('#c21235').move(40, 10)
-          draw.circle(50).fill('none').move(35, 5)
-              .stroke({ color: '#c21235', width: 5, linecap: 'round' })
-          draw.text(map.title).move(40, 60).font({weight: 'bold'})
-          draw.line(20, 100, 100, 100).move(60, 30)
-              .stroke({ color: 'rgba(194,18,53,0.68)', width: 10 })
+        const btnCicrle = (title) => {
+            draw.circle(40).fill('#c21235').move(40, 280)
+            draw.circle(50).fill('none').move(35, 280 - 5)
+                .stroke({color: '#c21235', width: 5, linecap: 'round'})
+            draw.text(title).move(40, 340).font({weight: 'bold'})
         }
 
-        if (i !== 0 && i <=3) {
-          draw.circle(30).fill('#c21235').move(140 + ((i-1) * 120), 15)
-          draw.text(map.title).move(170 + ((i-1) * 120), 60).font({weight: 'bold', anchor: 'middle'})
-          i !== 3  ?
-          draw.line(0, 100, 120, 100).move(((i) * 120)+20 , 30)
-              .stroke({ color: 'rgba(194,18,53,0.68)', width: 10 })
-              : draw.line(0, 100, 80, 100).move(((i) * 120)+20 , 30)
-                  .stroke({ color: 'rgba(194,18,53,0.68)', width: 10 })
-        }
+        let f = 1
+        let borderLeft = true;
+        let lin = 0;
+        cd.map((map, i) => {
+            if (i === 0) {
+                draw.circle(40).fill('#c21235').move(40, 10)
+                draw.circle(50).fill('none').move(35, 5)
+                    .stroke({color: '#c21235', width: 5, linecap: 'round'})
+                draw.text(map.title).move(40, 60).font({weight: 'bold'})
+            }
 
-        if (((i-1) % 3) % 2 === 1 )  {
-          const path = draw.path('M0,0 h20 a20, 20 0 0 1 20, 20 v100 a20, 20 0 0 1 -20, 20 h-20 a20, -20 v-200 a20, 20 0 0 1 20,-20 z')
-          path.fill('none').move(460, 30)
-          path.stroke({ color: 'rgba(194,18,53,0.68)', width: 10})
+            if (i !== 0 && borderLeft) {
+                draw.circle(30).fill('#c21235').move(200 + ((i - 1) * 210), !lin ? 15 : (lin * 90))
+                draw.text(map.title).move(210 + ((i - 1) * 220), 60).font({weight: 'bold', anchor: 'middle'})
+                if (f === 3) {
+                    f = 0;
+                    borderLeft = false;
+                    lin++
+                }
+            }
 
-          draw.line(20, 100, 420, 100).move(60, 170)
-              .stroke({ color: 'rgba(194,18,53,0.68)', width: 10 })
-        }
-        if (i > 3 && i <=6) {
-          draw.circle(30).fill('#c21235').move(40+((i%3)*180), 155)
-          draw.text(map.title).move(30+((i%3)*170),190).font({weight: 'bold'})
-        }
-
-        if (((i-1) % 3) % 2 === 0 )  {
-          draw.path('M0,0 h40 a20,20 0 0 1 20,20 v100 a20,20 0 0 1 -20,20 h-40 a20,20 0 0 1 20, z')
-              .fill('none').move(10, 170).rotate(180)
-              .stroke({ color: 'rgba(194,18,53,0.68)', width: 10 })
-        }
-
-        if (cd.length === i) {
-          draw.circle(40).fill('#c21235').move(40, (i % 3 * 120))
-          draw.circle(50).fill('none').move(35, 5)
-              .stroke({ color: '#c21235', width: 5, linecap: 'round' })
-          draw.text(map.title).move(40, 60).font({weight: 'bold'})
-          draw.line(20, 100, 100, 100).move(60, 30)
-              .stroke({ color: 'rgba(194,18,53,0.68)', width: 10 })
-        }
-
-      })
-
-
-
-
-
-
+            if (!borderLeft) {
+                draw.circle(30).fill('#c21235').move(80 + ((i % 3) * 190), 155)
+                draw.text(map.title).move(70 + ((i % 3) * 180), 190).font({weight: 'bold'})
+                if (f === 3) {
+                    f = 0;
+                    borderLeft = true
+                    lin++
+                }
+            }
+            f++;
+        })
 
 
     }
 
-  render() {
+    render() {
         return (
             <div className="App">
-            <div className="intercity">
+                <div className="intercity">
 
-            </div>
+                </div>
 
             </div>
         );
     }
 }
-
-
 
 
 export default App;
